c1cfc08589f31664b7fede0e79001c16e71b37e5
sc10624 <ceayon05@gmail.com>
2024-05-07 14:59:39 -0400

    worked on maps & camera height
    figured out how to have different maps for different rounds, fixed the camera height so it doesn't move with the character jumping

--- sketch.js
+++ sketch.js
@@ -3,17 +3,20 @@
 //put keys in an array and randomly assign them to fuctionalities (ex. move left, right, etc.) each time the character gets damaged (shuffle without overlapping)
 //make obstacles & items
 //build the map more (want to make multiple rounds but don't know how to have separate maps for each round)
-// **DIFFERENT LEVELS?**
 //turn hard numbers into variables
 //add more functionalities (crouching, etc.)
+//add visual effect for dying
 
-//** work on switching between maps of different rounds (somehow make platforms & ground work together to easily switch between rounds)
-// + the performance slowed down at some point but don't know why
 
 //********LOCK THE ROTATION OF THE PLAYER SQUARE*********
+//******************CONTROL KEYS NOT WORKING IN THE SECOND ROUND AFTER TOUCHING THE 1ST ROUND ENDPOINT (works when I switched to the second round with mouse tho)**********************
+//******SOMETIMES PLAYER RANDOMLY ACCELERATES?******
 
 //----screens---
-let screens = ["start", "menu", "roundComplete", "gameOver", "endScreen"];
+//let screens = ["start", "menu", "roundComplete", "gameOver", "endScreen"];
+
+//----camera----
+let camHeight = 40;
 
 //----round----
 let round;
@@ -27,7 +30,7 @@ let ground;
 let endPoint;
 
 //----keys array----
-let keys = ['a', 'd', 'space']; // maybe I need to convert these into keycodes
+//let keys = ['a', 'd', 'space']; // maybe I need to convert these into keycodes
 
 
 
@@ -44,45 +47,18 @@ function setup() {
 	round = 1;
 	
 	//----player----
-	player = new Sprite(20,100,5,5);
+	player = new Sprite(20,50,5,5);
 	player.color = "yellow";
 
-	//-----ROUND 1 group for ground tiles (map)---- //** CHANGE THE WAY THE VARIABLE NAME WORKS
-	ground = new Group();
-	ground.w = 10;
-	ground.h = 10;
-	ground.color = "green";
-	ground.tile = "=";
-	ground.collider = "static";
-	
-	//----platform side detectors for preventing sprite from jumping off the sides **IFFY (not completely "attached" to the sides, kinda floating in the middle)----
-	/*
-	sideDetectors = new Group();
-	sideDetectors.w = 1;
-	sideDetectors.h = 10;
-	sideDetectors.visible = false;
-	sideDetectors.tile = "|";
-	sideDetectors.collider = "static";
-	*/
+	//-----round 1 ground tiles---- (separate ground tiles for each round bc of colliding detection (ground tiles from other round should have 'none' collider))
+	r1ground = new Group();
+	r1ground.w = 10;
+	r1ground.h = 10;
+	r1ground.color = "green";
+	r1ground.tile = "=";
+	r1ground.collider = "static";
 	
 	r1map = new Tiles(
-		/* (using sideDetectors)
-		['.......................................................................................',
-		 '.......................................................................................',
-		 '.......................................................................................',
-		 '.......................................................................................',
-		 '.......................................................................................',
-		 '.......................................................................................',
-		 '=====|...|===..=======...===..=======...===..=======...===..=======...===..==....===..=',
-		 '=====|...|===..=======...===..=======...===..=======...===..=======...===..==....===..=',
-		 '=====|...|===..=======...===..=======...===..=======...===..=======...===..==....===..=',
-		 '=====|...|===..=======...===..=======...===..=======...===..=======...===..==....===..=',
-		 '=====|...|===..=======...===..=======...===..=======...===..=======...===..==....===..=',
-		 '=====|...|===..=======...===..=======...===..=======...===..=======...===..==....===..=',
-		 '=====|...|===..=======...===..=======...===..=======...===..=======...===..==....===..=',
-		 ],
-		 */
-		
 		//without sideDetectors
 		['.....................................................................................',
 		 '.....................................................................................',
@@ -98,20 +74,50 @@ function setup() {
 		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
 		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
 		 ],
+		0, //top left corner
 		0,
-		50,
-		ground.w,
-		ground.h
+		r1ground.w, //spacing
+		r1ground.h
 	)
+	
+	r2ground = new Group();
+	r2ground.w = 10;
+	r2ground.h = 10;
+	r2ground.color = "brown";
+	r2ground.tile = "-";
+	r2ground.collider = "static";
+	
+	r2map = new Tiles(
+		['.....................................................................................',
+		 '.....................................................................................',
+		 '.....................................................................................',
+		 '.....................................................................................',
+		 '.....................................................................................',
+		 '.....................................................................................',
+		 '---------..--------...---..-------------..-------...------------...---..--....---..-',
+		 '---------..--------...---..-------------..-------...------------...---..--....---..-',
+		 '---------..--------...---..-------------..-------...------------...---..--....---..-',
+		 '---------..--------...---..-------------..-------...------------...---..--....---..-',
+		 '---------..--------...---..-------------..-------...------------...---..--....---..-',
+		 '---------..--------...---..-------------..-------...------------...---..--....---..-',
+		 '---------..--------...---..-------------..-------...------------...---..--....---..-',
+		 ],
+		0, //top left corner
+		0,
+		r2ground.w, //spacing
+		r2ground.h
+	)
+	
+	
 	
 	//----platforms----
 	let platforms = new Group();
-	platforms.width = 15;
+	platforms.width = 25;
 	platforms.height = 5;
 	platforms.color = "orange";
 	platforms.collider = "static";
 	
-	let p1 = new platforms.Sprite(115,80);
+	let p1 = new platforms.Sprite(160,30); //just testing
 	
 	//----obstacles----
 	let obstacles = new Group();
@@ -127,7 +133,7 @@ function setup() {
 	*/
 	
 	//----endPoint----
-	endPoint = new Sprite(780,90,10,10);
+	endPoint = new Sprite(790,30,10,10);
 	endPoint.color = "purple";
 	endPoint.collider = 'static';
 	
@@ -142,23 +148,63 @@ function draw() {
 	clear();
 	background("skyblue"); //will change later
 	
+	// ROUND 1
 	if(round == 1){
-		r1map.visible = true; //** want to make r1map appear again after removeAll() has been called...
+		r1ground.collider = "static";
+		r1map.visible = true;
+		
+		r2ground.collider = "none";
+		r2map.visible = false;
+		
+    //works, but TOO LAGGY
+		/*
+		new Tiles(
+		['.....................................................................................',
+		 '.....................................................................................',
+		 '.....................................................................................',
+		 '.....................................................................................',
+		 '.....................................................................................',
+		 '.....................................................................................',
+		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
+		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
+		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
+		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
+		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
+		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
+		 '=====...===..=======...===..=======...===..=======...===..=======...===..==....===..=',
+		 ],
+		0, //top left corner
+		0,
+		ground.w, //spacing
+		ground.h
+	)
+	*/
 		setCam();
 		leftLimit();
 		movePlayer();
 		resetPlayer();
-		endGame();
+		endRound();
 	}
-	else{
-		r1map.removeAll();
+	
+	
+	// ROUND 2 (somehow round is set to 3 & player control doesn't work after 1st round is cleared... but then round 2 gets displayed??)
+	if(round == 2){
+		r1ground.collider = "none";
+		r1map.visible = false;
+		
+		r2ground.collider = "static";
+		r2map.visible = true;
+		
 		setCam();
 		leftLimit();
 		movePlayer();
 		resetPlayer();
-		endGame();
+		endRound();
+
+		//print(kb.pressing('a')); seems like 'a' key is not even "pressed"? (but works when I switched to round 2 with mouse click)
 	}
 
+	print(round);
 	
 }// draw ends
 
@@ -172,33 +218,61 @@ function setCam(){ //set the camera **maybe constrain cam x so there's no visibl
 	
 	camera.zoom = 6;
 	camera.x = player.x + 45;
-	camera.y = player.y - 10;
+	camera.y = camHeight;
 	
 }//setCam ends
 
 
-function movePlayer(){ //move player with keyboard ***PLAYER CAN JUMP WHEN TOUCHING THE SIDE OF THE GROUND BLOCKS***
+function movePlayer(){ //move player with keyboard ***PLAYER CAN JUMP WHEN TOUCHING THE SIDEs OF THE GROUND BLOCKS***
 	
+	if (round == 1){
+	//----round 1----
 		if(kb.pressing('a')){ // nested jump function inside left/right movements for multiple keys working together
-			if(kb.pressing('space') && player.colliding(ground)){
+			if(kb.pressing('space') && player.colliding(r1ground)){
 				player.vel.y = -3;
-				player.x -= 2;
+				player.x -= 1.5;
 			}
 			else{
-				player.x -= 2;
+				player.x -= 1.5;
 			}
-	}
-	else if(kb.pressing('d')){
-			if(kb.pressing('space') && player.colliding(ground)){
+		}
+		else if(kb.pressing('d')){
+				if(kb.pressing('space') && player.colliding(r1ground)){
+					player.vel.y = -3;
+					player.x -= 1.5;
+				}
+			else{
+			player.x += 1.5;
+			}
+		}
+		else if(kb.pressing('space') && player.colliding(r1ground)){ //for vertical jump
 				player.vel.y = -3;
-				player.x -= 2;
-			}
-		else{
-		player.x += 2;
 		}
 	}
-	else if(kb.pressing('space') && player.colliding(ground)){ //for vertical jump
-			player.vel.y = -3;
+	
+	//----round 2----
+	if (round == 2){
+		if(kb.pressing('a')){ // nested jump function inside left/right movements for multiple keys working together
+			if(kb.pressing('space') && player.colliding(r2ground)){
+				player.vel.y = -3;
+				player.x -= 1.5;
+			}
+			else{
+				player.x -= 1.5;
+			}
+		}
+		else if(kb.pressing('d')){
+			if(kb.pressing('space') && player.colliding(r2ground)){
+				player.vel.y = -3;
+				player.x -= 1.5;
+			}
+			else{
+			player.x += 1.5;
+			}
+		}
+		else if(kb.pressing('space') && player.colliding(r2ground)){ //for vertical jump
+				player.vel.y = -3;
+		}
 	}
 	
 }//movePayer ends
@@ -212,23 +286,32 @@ function resetPlayer(){ //return player to origin if fell down
 		player.vel.x = 0; // sets the velocity to 0 each time, preventing the sprite from gaining too much force
 		player.vel.y = 0;
 	}	
-	
-	
 
 }//resetPlayer ends
 
 
-function endGame(){ // end game when reached endpoint (display a screen indicating round completed)
+function endRound(){ // end game when reached endpoint (display a screen indicating round completed)
 	
-	if(player.colliding(endPoint)){ //set it to original point for now just to check
+	if(player.collided(endPoint)){ //set it to original point for now just to check
 		
 		//****also set the turnback point to round 1 just for checking how the code works, make it display ending screen later
+		
+		/*
+		round++;
+	
+		if(round > roundMax){
+			round = 1;
+		}
+		*/
+		
+		///*
 		if(round <= roundMax){
 		  round += 1;
 		}
 		else{
 			round = 1;
 		}
+		//*/
 
 		player.x = 20;
 		player.y = 50;
@@ -236,7 +319,7 @@ function endGame(){ // end game when reached endpoint (display a screen indicati
 		player.vel.y = 0;
 	}
 	
-}//endGame ends
+}//endRound ends
 
 
 function leftLimit(){ // player can't go beyond the left end of the screen (it kinda prevens the weird gravity when the player position is reset)
@@ -251,10 +334,13 @@ function leftLimit(){ // player can't go beyond the left end of the screen (it k
 
 // ** for checking real quick if showing/removing of r1map works properly
 function mousePressed(){
+	///*
 	round++;
+	
 	if(round > roundMax){
 	  round = 1;
 	}
+	//*/
 	
 	print(round);
 }
